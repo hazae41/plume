a6d7c1fb606bf01e3738be198cbbcc89e1cbda78
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-11 14:46:58 +0200

    export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export * from "./libs/abort/abort.js";
+export * from "./libs/cleanable/cleanable.js";
 export * from "./mods/index.js";
 export * as Plume from "./mods/index.js";
 
--- src/mods/index.ts
+++ src/mods/index.ts
@@ -1,4 +1,4 @@
-export * from "../libs/abort/abort.js"
 export * from "./errors.js"
 export * from "./target.js"
+export * from "./waiters.js"
 
