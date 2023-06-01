90410e407278c54d7cece13c484bc2d9d82827f6
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-06-01 21:14:28 +0200

    errors

--- src/mods/waiters.ts
+++ src/mods/waiters.ts
@@ -5,17 +5,25 @@ import { Promiseable } from "libs/promises/promiseable.js"
 import { AbortError, CloseError, ErrorError } from "./errors.js"
 import { SuperEventTarget } from "./target.js"
 
+export type WaitError =
+  | AbortError
+
 export async function tryWaitOrSignal<M, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: (e: M[K]) => Promiseable<Result<Option<Ok<R>>, unknown>>, signal: AbortSignal) {
   const abort = AbortError.wait(signal)
   const event = target.wait(type, callback)
 
-  return await Cleaner.race<Result<R, AbortError>>([abort, event])
+  return await Cleaner.race<Result<R, WaitError>>([abort, event])
 }
 
 export type StreamEvents = {
   close: unknown,
   error: unknown
 }
+
+export type WaitStreamError =
+  | AbortError
+  | CloseError
+  | ErrorError
 
 export async function tryWaitOrStream<M extends StreamEvents, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: (e: M[K]) => Promiseable<Result<Option<Ok<R>>, unknown>>) {
   const error = ErrorError.wait(target)
@@ -31,5 +39,5 @@ export async function tryWaitOrStreamOrSignal<M extends StreamEvents, K extends 
   const close = CloseError.wait(target)
   const event = target.wait(type, callback)
 
-  return await Cleaner.race<Result<R, AbortError | ErrorError | CloseError>>([abort, error, close, event])
+  return await Cleaner.race<Result<R, WaitStreamError>>([abort, error, close, event])
 }
