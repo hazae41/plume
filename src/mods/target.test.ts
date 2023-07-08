db6405beb6c5ffb80bd763618a026d69a0df3b85
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-07-08 15:19:42 +0200

    wait

--- src/mods/target.test.ts
+++ src/mods/target.test.ts
@@ -11,7 +11,7 @@ console.log(relative(directory, pathname.replace(".mjs", ".ts")))
 
 Debug.debug = true
 
-test("AsyncEventTarget", async ({ test }) => {
+test("AsyncEventTarget", async ({ test, wait }) => {
   const target = new SuperEventTarget<{
     test: (order: "first" | "second") => number
   }>()
@@ -19,16 +19,17 @@ test("AsyncEventTarget", async ({ test }) => {
   const stack = new Array<string>()
 
   target.on("test", async (order) => {
+    console.log("on first", order)
+
     if (order !== "first")
       return new None()
 
-    console.log("first", order)
     stack.push(order)
     return new Some(123)
   }, { passive: true })
 
   target.on("test", async (order) => {
-    console.log("second", order)
+    console.log("on second", order)
 
     if (order !== "second")
       return new None()
@@ -44,26 +45,32 @@ test("AsyncEventTarget", async ({ test }) => {
       return new Some(new Ok(order))
     }, signal).then(r => r.unwrap())
 
-    console.log("got", first)
+    console.log("wait first", first)
+
+    const signal2 = AbortSignal.timeout(1000)
 
     const second = await tryWaitOrSignal(target, "test", order => {
       return new Some(new Ok(order))
-    }, signal).then(r => r.unwrap())
+    }, signal2).then(r => r.unwrap())
 
-    console.log("got", second)
+    console.log("wait second", second)
   })
 
-  // await new Promise(ok => setTimeout(ok, 1000))
+  await new Promise(ok => setTimeout(ok, 100))
 
   const first = await target.emit("test", ["first"])
-  console.log("returned", first)
+  console.log("emit first", first)
   assert(first.isSome(), "Event has not been handled")
 
+  await new Promise(ok => setTimeout(ok, 100))
+
   const second = await target.emit("test", ["second"])
-  console.log("returned", second)
+  console.log("emit second", second)
   assert(second.isSome(), "Event has not been handled")
 
   stack.push("done")
+
+  await wait()
 
   assert(stack.length === 3)
   assert(stack[0] === "first")
--- src/mods/target.ts
+++ src/mods/target.ts
@@ -47,7 +47,7 @@ export class SuperEventTarget<M extends Record<string, (...args: any) => any>> {
   /**
    * Add a listener to an event
    * @param type Event type //  "abort", "error", "message", "close"
-   * @param listener Event listener // (e) => console.log("hello")
+   * @param listener Event listener // (e) => new Some(123)
    * @param options Options // { passive: true }
    * @returns 
    */
@@ -100,11 +100,12 @@ export class SuperEventTarget<M extends Record<string, (...args: any) => any>> {
    * Dispatch an event to its listeners
    * 
    * - Dispatch to active listeners sequencially
-   * - Return false if the event has been cancelled
+   * - Return if one of the listeners returned something
    * - Dispatch to passive listeners concurrently
-   * - Return true
+   * - Return if one of the listeners returned something
+   * - Return nothing
    * @param value The object to emit
-   * @returns `Ok(value)` or `Err<EventError>`
+   * @returns `Some` if the event 
    */
   async emit<K extends keyof M>(type: K, value: Parameters<M[K]>): Promise<Option<ReturnType<M[K]>>> {
     const listeners = this.#listeners.get(type) as Map<SuperEventListener<M[K]>, InternalSuperEventListenerOptions> | undefined
@@ -156,6 +157,12 @@ export class SuperEventTarget<M extends Record<string, (...args: any) => any>> {
     return new None()
   }
 
+  /**
+   * Like `.on`, but instead of returning to the target, capture the returned value in a future, and return nothing to the target
+   * @param type 
+   * @param callback 
+   * @returns 
+   */
   wait<K extends keyof M, R>(type: K, callback: SuperEventWaiter<M[K], R>) {
     const future = new Future<R>()
 
