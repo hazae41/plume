fbfcf65c1d7ca3471c05b50156153980a8cde445
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-07-08 15:31:17 +0200

    clean

--- src/mods/waiters.ts
+++ src/mods/waiters.ts
@@ -3,25 +3,12 @@ import { Ok, Result } from "@hazae41/result"
 import { AbortedError, CloseEvents, ClosedError, ErrorEvents, ErroredError } from "./errors.js"
 import { SuperEventMap, SuperEventTarget, SuperEventWaiter } from "./target.js"
 
-export type WaitError =
-  | AbortedError
-
 export async function tryWaitOrSignal<M extends SuperEventMap, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], Ok<R>>, signal: AbortSignal) {
   const abort = AbortedError.wait(signal)
   const event = target.wait(type, callback)
 
-  return await Cleaner.race<Promise<Result<R, WaitError>>>([abort, event])
+  return await Cleaner.race<Promise<Result<R, AbortedError>>>([abort, event])
 }
-
-export type StreamEvents = {
-  close: (event: unknown) => void,
-  error: (event: unknown) => void
-}
-
-export type WaitStreamError =
-  | AbortedError
-  | ClosedError
-  | ErroredError
 
 export async function tryWaitOrCloseOrError<M extends CloseEvents & ErrorEvents, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], Ok<R>>) {
   const error = ErroredError.wait(target)
@@ -37,5 +24,5 @@ export async function tryWaitOrCloseOrErrorOrSignal<M extends CloseEvents & Erro
   const close = ClosedError.wait(target)
   const event = target.wait(type, callback)
 
-  return await Cleaner.race<Promise<Result<R, WaitStreamError>>>([abort, error, close, event])
+  return await Cleaner.race<Promise<Result<R, AbortedError | ClosedError | ErroredError>>>([abort, error, close, event])
 }
