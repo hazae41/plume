91cda0fcee3130b7fec96b8857571f4652af6d5e
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-03-09 19:06:51 +0100

    errors

--- src/mods/errors.ts
+++ src/mods/errors.ts
@@ -1,18 +1,25 @@
 import { Disposer } from "@hazae41/disposer"
 import { Future } from "@hazae41/future"
 import { None } from "@hazae41/option"
-import { Err } from "@hazae41/result"
 import { SuperEventTarget } from "./target.js"
 
 export class AbortedError extends Error {
   readonly #class = AbortedError
   readonly name = this.#class.name
 
-  static from(cause: unknown) {
-    return new AbortedError(`Aborted`, { cause })
+  constructor(options?: ErrorOptions) {
+    super("Aborted", options)
   }
 
-  static waitOrThrow(signal: AbortSignal) {
+  static from(cause: unknown) {
+    return new AbortedError({ cause })
+  }
+
+}
+
+export namespace AbortSignals {
+
+  export function waitOrThrow(signal: AbortSignal) {
     const future = new Future<never>()
 
     if (signal.aborted) {
@@ -31,77 +38,60 @@ export class AbortedError extends Error {
     return new Disposer(promise, dispose)
   }
 
-  static tryWait(signal: AbortSignal) {
-    const future = new Future<Err<AbortedError>>()
-
-    if (signal.aborted) {
-      future.resolve(new Err(AbortedError.from(signal)))
-      return new Disposer(future.promise, () => { })
-    }
-
-    const onAbort = (event: Event) => {
-      future.resolve(new Err(AbortedError.from(event)))
-    }
-
-    signal.addEventListener("abort", onAbort, { passive: true })
-    const dispose = () => signal.removeEventListener("abort", onAbort)
-    const promise = future.promise.finally(dispose)
-
-    return new Disposer(promise, dispose)
-  }
-
-}
-
-export type ErrorEvents = {
-  error: (reason: unknown) => void
 }
 
 export class ErroredError extends Error {
   readonly #class = ErroredError
   readonly name = this.#class.name
 
-  static from(cause: unknown) {
-    return new ErroredError(`Errored`, { cause })
+  constructor(options?: ErrorOptions) {
+    super("Errored", options)
   }
 
-  static waitOrThrow<M extends ErrorEvents>(target: SuperEventTarget<M>) {
+  static from(cause: unknown) {
+    return new ErroredError({ cause })
+  }
+
+}
+
+export type ErrorEvents = {
+  readonly error: (reason: unknown) => void
+}
+
+export namespace ErrorEvents {
+
+  export function waitOrThrow<M extends ErrorEvents>(target: SuperEventTarget<M>) {
     return target.wait("error", (future: Future<never>, event) => {
       future.reject(ErroredError.from(event))
       return new None()
     })
   }
 
-  static tryWait<M extends ErrorEvents>(target: SuperEventTarget<M>) {
-    return target.wait("error", (future: Future<Err<ErroredError>>, event) => {
-      future.resolve(new Err(ErroredError.from(event)))
-      return new None()
-    })
-  }
-
-}
-
-export type CloseEvents = {
-  close: (reason: unknown) => void
 }
 
 export class ClosedError extends Error {
   readonly #class = ClosedError
   readonly name = this.#class.name
 
-  static from(cause: unknown) {
-    return new ClosedError(`Closed`, { cause })
+  constructor(options?: ErrorOptions) {
+    super("Closed", options)
   }
 
-  static waitOrThrow<M extends CloseEvents>(target: SuperEventTarget<M>) {
+  static from(cause: unknown) {
+    return new ClosedError({ cause })
+  }
+
+}
+
+export type CloseEvents = {
+  readonly close: (reason: unknown) => void
+}
+
+export namespace CloseEvents {
+
+  export function waitOrThrow<M extends CloseEvents>(target: SuperEventTarget<M>) {
     return target.wait("close", (future: Future<never>, event) => {
       future.reject(ClosedError.from(event))
-      return new None()
-    })
-  }
-
-  static tryWait<M extends CloseEvents>(target: SuperEventTarget<M>) {
-    return target.wait("close", (future: Future<Err<ClosedError>>, event) => {
-      future.resolve(new Err(ClosedError.from(event)))
       return new None()
     })
   }
--- src/mods/waiters.ts
+++ src/mods/waiters.ts
@@ -1,50 +1,25 @@
-import { Ok, Result } from "@hazae41/result"
-import { AbortedError, CloseEvents, ClosedError, ErrorEvents, ErroredError } from "./errors.js"
+import { AbortSignals, CloseEvents, ErrorEvents } from "./errors.js"
 import { SuperEventMap, SuperEventTarget, SuperEventWaiter } from "./target.js"
 
 export async function waitOrSignal<M extends SuperEventMap, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], R>, signal: AbortSignal): Promise<R> {
-  using abort = AbortedError.waitOrThrow(signal)
-  using event = target.wait(type, callback)
-
-  return await Promise.race([abort.get(), event.get()])
-}
-
-export async function tryWaitOrSignal<M extends SuperEventMap, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], Ok<R>>, signal: AbortSignal): Promise<Result<R, Error>> {
-  using abort = AbortedError.tryWait(signal)
+  using abort = AbortSignals.waitOrThrow(signal)
   using event = target.wait(type, callback)
 
   return await Promise.race([abort.get(), event.get()])
 }
 
 export async function waitOrCloseOrError<M extends SuperEventMap & CloseEvents & ErrorEvents, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], R>): Promise<R> {
-  using error = ErroredError.waitOrThrow(target)
-  using close = ClosedError.waitOrThrow(target)
-  using event = target.wait(type, callback)
-
-  return await Promise.race([error.get(), close.get(), event.get()])
-}
-
-export async function tryWaitOrCloseOrError<M extends SuperEventMap & CloseEvents & ErrorEvents, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], Ok<R>>): Promise<Result<R, Error>> {
-  using error = ErroredError.tryWait(target)
-  using close = ClosedError.tryWait(target)
+  using error = ErrorEvents.waitOrThrow(target)
+  using close = CloseEvents.waitOrThrow(target)
   using event = target.wait(type, callback)
 
   return await Promise.race([error.get(), close.get(), event.get()])
 }
 
 export async function waitOrCloseOrErrorOrSignal<M extends SuperEventMap & CloseEvents & ErrorEvents, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], R>, signal: AbortSignal): Promise<R> {
-  using abort = AbortedError.waitOrThrow(signal)
-  using error = ErroredError.waitOrThrow(target)
-  using close = ClosedError.waitOrThrow(target)
-  using event = target.wait(type, callback)
-
-  return await Promise.race([abort.get(), error.get(), close.get(), event.get()])
-}
-
-export async function tryWaitOrCloseOrErrorOrSignal<M extends SuperEventMap & CloseEvents & ErrorEvents, K extends keyof M, R>(target: SuperEventTarget<M>, type: K, callback: SuperEventWaiter<M[K], Ok<R>>, signal: AbortSignal): Promise<Result<R, Error>> {
-  using abort = AbortedError.tryWait(signal)
-  using error = ErroredError.tryWait(target)
-  using close = ClosedError.tryWait(target)
+  using abort = AbortSignals.waitOrThrow(signal)
+  using error = ErrorEvents.waitOrThrow(target)
+  using close = CloseEvents.waitOrThrow(target)
   using event = target.wait(type, callback)
 
   return await Promise.race([abort.get(), error.get(), close.get(), event.get()])
